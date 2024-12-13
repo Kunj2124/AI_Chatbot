a90846a64dba5ed2860eddaa5b5eb287488ab99b
Kunj Agarwal <130543586+Kunj2124@users.noreply.github.com>
2024-12-13 13:09:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,7 @@ function App() {
 
     try {
       const response = await axios({
-        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${
-          import.meta.env.VITE_API_GENERATIVE_LANGUAGE_CLIENT
-        }`,
+        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${AIzaSyBwCo7UhrJENdN6TfGjMur699aBk6v8q2A}`,
         method: "post",
         data: {
           contents: [{ parts: [{ text: question }] }],
